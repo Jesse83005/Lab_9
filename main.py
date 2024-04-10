93ce8e4d34a691e02119b31585df9ee03f358f6d
JPLellis <jplellis1@gmail.com>
2024-04-10 15:46:21 -0400

    Added decode func

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ def encode(num):
         final.append(x)
     return("".join(str(num) for num in final))
 
+def decode(stored_pass):
+    numlist = list(str(stored_pass))
+    intlist = []
+    final = []
+    for i in numlist:
+        x = int(i)
+        intlist.append(x)
+    for i in intlist:
+        x = i-3
+        final.append(x)
+    return("".join(str(num) for num in final))
+
 
 def menu():
     print("Menu")
@@ -27,7 +39,8 @@ if __name__ == "__main__":
             stored_pass = encode(num)
             print("Your password has been encoded and stored!\n")
         elif opt == "2":
-            pass
+            decoded_pass = decode(stored_pass)
+            print(f"The encoded password is {stored_pass}, and the original password is {decoded_pass}.")
         elif opt == "3":
             break
         else:
