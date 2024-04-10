703b79064a7de98ae0b696ca2dad71f2faf81928
Jesse83005 <jesse8305@outlook.com>
2024-04-10 14:52:07 -0400

    Main file

---
+++ main.py
@@ -0,0 +1,13 @@
+def encode(num):
+    pass
+
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+if __name__ == "__main__":
+    menu()
+    opt = input("Please enter an option: ")
